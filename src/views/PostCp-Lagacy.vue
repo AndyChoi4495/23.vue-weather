eb0ebec80afb25bfde0341179b5480500282954b
AndyChoi4495 <yun4495@gmail.com>
2021-10-13 14:21:51 +0900

    >>

---
+++ src/views/PostCp-Lagacy.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="posts-wrapper">
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <td>번호</td>
+          <td>제목</td>
+          <td>내용</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="v in posts" :key="v.id">
+          <td>{{ v.id }}</td>
+          <td>{{ v.title }}</td>
+          <td>{{ v.body }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  async created () {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+    const { data } = await axios.get(url)
+    this.posts = data
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.posts-wrapper {
+  width: 90%;
+  max-width: 720px;
+  margin: 2em auto;
+  overflow-y: auto;
+}
+</style>
---
+++ src/views/UsersCp.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="users-wrapper">
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <td>번호</td>
+          <td>이름</td>
+          <td>전화번호</td>
+          <td>이메일</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="v in lists" :key="v.id">
+          <td>{{ v.id }}</td>
+          <td>{{ v.name }}</td>
+          <td>{{ v.phone }}</td>
+          <td>{{ v.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+  props: ['lists']
+}
+</script>
+
+<style lang="scss" scoped>
+.users-wrapper {
+  width: 90%;
+  max-width: 720px;
+  margin: 2em auto;
+  overflow-y: auto;
+}
+</style>
